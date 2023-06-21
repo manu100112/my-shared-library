31e56e261a5a8c8901bee39c2ff0ada081bb7369
manu100112 <99679441+manu100112@users.noreply.github.com>
2023-06-21 19:17:26 +0530

    Update buildPipeline.groovy

--- src/buildPipeline.groovy
+++ src/buildPipeline.groovy
@@ -1,15 +1,6 @@
+// my-shared-library/vars/buildPipeline.groovy
 def call() {
-    pipeline {
-        agent any
-
-        stages {
-            stage('Build') {
-                steps {
-                    // Build steps for your project
-                    sh 'echo "Building the project"'
-                    sh 'mvn clean package'
-                }
-            }
-        }
-    }
+    // Build steps for your project
+    sh 'echo "Building the project"'
+    sh 'mvn clean package'
 }
